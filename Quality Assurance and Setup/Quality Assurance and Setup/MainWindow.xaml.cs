e768c16baf0e116c3b6ece7175d9869f552f19a1
J S <lamierus@gmail.com>
2016-10-24 11:22:14 -0400

    finally adding some controls to the UI.

---
+++ Quality Assurance and Setup/Quality Assurance and Setup/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quality_Assurance_and_Setup {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        static bool Is64bit = Environment.Is64BitOperatingSystem;
+
+        public MainWindow() {
+            InitializeComponent();
+            
+            if (Is64bit) {
+                lblWindowsVersion.Content = "64-Bit";
+            } else {
+                lblWindowsVersion.Content = "32-Bit";
+            }
+        }
+
+    }
+}
