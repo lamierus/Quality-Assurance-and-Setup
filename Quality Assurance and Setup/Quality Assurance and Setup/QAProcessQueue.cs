a5db7f53f79a8288361078288a2710a716880ec3
J S <lamierus@gmail.com>
2017-06-29 16:14:12 -0400

    adding more automated tasks. including: -a method for pinning/unpinning items to the taskbar. -creating a shortcut on the desktop.

--- Quality Assurance and Setup/Quality Assurance and Setup/QAProcessQueue.cs
+++ Quality Assurance and Setup/Quality Assurance and Setup/QAProcessQueue.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IWshRuntimeLibrary;
+using Shell32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,25 +25,36 @@ namespace Quality_Assurance_and_Setup {
         }
 
         private void initializeFullQueue() {
-            if (X64) {
-                /*
-                ::64-BIT SPECIFIC
-                ::Create a Junos Pulse link on the Desktop
-                echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-                echo sLinkFile = "C:\Users\Public\Desktop\Pulse Secure.lnk" >> CreateShortcut.vbs
-                echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-                echo oLink.TargetPath = "C:\Program Files (x86)\Common Files\Juniper Networks\JamUI\Pulse.exe" >> CreateShortcut.vbs
-                echo oLink.Arguments = "-show" >> CreateShortcut.vbs
-                echo oLink.Save >> CreateShortcut.vbs
-                cscript CreateShortcut.vbs
-                del CreateShortcut.vbs
 
-                ::Start Junos Pulse to test
-                start "" "C:\Program Files (x86)\Common Files\Juniper Networks\JamUI\Pulse.exe" -show
-                */
+            object shDesktop = (object)"Desktop";
+            WshShell shell = new WshShell();
+            string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\Pulse Secure.lnk";
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Arguments = "-show";
+
+            QAProcess StartVPN = new QAProcess("Start VPN",
+                                               "Starts the VPN software for testing purposes");
+            if (!X64) {
+                //32-bit specific
+
+                //setting all of the path information for the VPN link to be placed on the desktop
+                shortcut.TargetPath = @"C:\Program Files\Common Files\Juniper Networks\JamUI\Pulse.exe";
+                shortcut.WorkingDirectory = @"C:\Program Files\Common Files\Juniper Networks\JamUI";
+
+                //all of these \'s are required for escapes on the " and \ symbols in the command
+                StartVPN.Script = "\"\" \"C:\\Program Files\\Common Files\\Juniper Networks\\JamUI\\Pulse.exe\" -show";
             } else {
-                //add 32-bit specific stuff here
+                //64-bit specific
+
+                //setting all of the path information for the VPN link to be placed on the desktop
+                shortcut.TargetPath = @"C:\Program Files (x86)\Common Files\Juniper Networks\JamUI\Pulse.exe";
+                shortcut.WorkingDirectory = @"C:\Program Files (x86)\Common Files\Juniper Networks\JamUI";
+
+                StartVPN.Script = "\"\" \"C:\\Program Files (x86)\\Common Files\\Juniper Networks\\JamUI\\Pulse.exe\" -show";
             }
+
+            shortcut.Save();
+            ProcessQueue.Add(StartVPN);
 
             //initialize the Lync process with the standard part --
             QAProcess StartLync = new QAProcess("Start Lync",
@@ -54,53 +67,83 @@ namespace Quality_Assurance_and_Setup {
             }
             ProcessQueue.Add(StartLync);
 
+            //NON-SPECIFIC
+            //Run TagIT to set the asset tag #
+            QAProcess RunTagIT = new QAProcess("Start TagIT",
+                                               "Run TagIT to set the asset tag #",
+                                               "\"\" \"C:\\Program Files\\Marimba\\AddOns\\TagIT.exe\"");
+            
             /*
-            ::NON-SPECIFIC
-            ::Run TagIT to set the asset tag #
-            start "" "C:\Program Files\Marimba\AddOns\TagIT.exe"
-
             ::Remove the superfluous links, incase they were migrated
             del /f "C:\Users\Public\Desktop\Avaya VPN Client.lnk"
             del /f "C:\Users\Public\Desktop\MyPassword.lnk"
             del /f "C:\Users\Public\Desktop\Adobe Reader*.lnk"
-
+            /*
             ::merge in the IE security zone and Lync fixes
             start wscript.exe /e:vbscript ".\QA Scripts\IEHealing.vbs"
             start regedit.exe /s ".\QA Scripts\Lync Installing Repeat Fix.reg"
-
+            /*
             ::Use the VB script to pin a Lync 2010 icon to the taskbar
             cscript ".\QA Scripts\PinItem.vbs" /taskbar /item:"%ProgramData%\Microsoft\Windows\Start Menu\Programs\Microsoft Lync\Microsoft Lync 2010.lnk" /q
             cscript ".\QA Scripts\PinItem.vbs" /taskbar /item:"%ProgramData%\Microsoft\Windows\Start Menu\Programs\Microsoft Office 2013\Lync 2013.lnk" /q
             cscript ".\QA Scripts\PinItem.vbs" /taskbar /item:"%ProgramData%\Microsoft\Windows\Start Menu\Programs\Skype for Business 2016.lnk" /q
 
+            /*
             ::Start Channel Viewer to make sure everything is installed correctly
             start wscript.exe C:\HP\Scripts\StartChannelViewer.VBS
-
+            /*
             ::Start IE to check Favorites and send to the Xerox MPS Portal
             start iexplore http://mpsportal.pg.com
-
+            /*
             ::Open the Old Printer Info text to add the user's default printer
             explorer C:\Users\Public\Desktop\Printer Info\Old Printer Information.txt
-
+            /*
             ::Open the Documents folder to verify transfer of documents
             explorer C:\Users\%userprofile%\Documents\
-
+            /*
             ::Start Certificate Manager to remove the old junos pulse certificate, if required
             start certmgr.msc
-
+            /*
             ::Start Excel to clear any errors on first run
             start "" "excel.exe" /m
-
+            /*
             ::Start Outlook to verify transfer and sync with Exchange
             start outlook.exe
-
+            /*
             ::Run all of the updates
             start "" "C:\Windows\System32\TuneUp\TuneUp.exe" /RunNowAll
             */
 
+        }
+
+        private static void PinUnpinTaskBar(string filePath, bool pin) {
+            if (!System.IO.File.Exists(filePath)) throw new System.IO.FileNotFoundException(filePath);
+
+            // create the shell application object
+            Shell shellApplication = new Shell();
+
+            string path = System.IO.Path.GetDirectoryName(filePath);
+            string fileName = System.IO.Path.GetFileName(filePath);
+
+            Shell32.Folder directory = shellApplication.NameSpace(path);
+            FolderItem link = directory.ParseName(fileName);
+
+            FolderItemVerbs verbs = link.Verbs();
+            for (int i = 0; i < verbs.Count; i++) {
+                FolderItemVerb verb = verbs.Item(i);
+                string verbName = verb.Name.Replace(@"&", string.Empty).ToLower();
+
+                if ((pin && verbName.Equals("pin to taskbar")) || (!pin && verbName.Equals("unpin from taskbar"))) {
+
+                    verb.DoIt();
+                }
             }
 
-            public void executeQueue(MainWindow siht) {
+            shellApplication = null;
+        }
+
+
+        public void executeQueue(MainWindow siht) {
             foreach (QAProcess QAp in ProcessQueue) {
                 try {
                     if (!QAp.runScript()) {
